c5fd4a9c98b6704d79a5dccd866b06712073c928
Eoghain O'Briain <eoghain.obriain@appliedtechnology.se>
2023-08-30 16:14:43 +0200

    start

--- src/components/elements/selection.tsx
+++ src/components/elements/selection.tsx
@@ -1,12 +1,84 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Selection = () => {
+  const [type, setType] = useState("addition");
+  const [difficulty, setDifficulty] = useState("easy");
+
+  const handleTypeChange = (e: any) => {
+    setType(e.target.value);
+  };
+
+  const handleDifficultyChange = (e: any) => {
+    setDifficulty(e.target.value);
+  };
+
   return (
-    <form>
-      <button>Easy</button>
-      <button>Medium</button>
-      <button>Hard</button>
-    </form>
+    <>
+      <div>
+        <label>Math Type</label>
+        <br />
+        <input
+          type="radio"
+          value="addition"
+          name="mathtype"
+          checked={type === "addition"}
+          onChange={handleTypeChange}
+        />
+        Addition
+        <input
+          type="radio"
+          value="subtraction"
+          name="mathtype"
+          checked={type === "subtraction"}
+          onChange={handleTypeChange}
+        />
+        Subtraction
+        <input
+          type="radio"
+          value="division"
+          name="mathtype"
+          checked={type === "division"}
+          onChange={handleTypeChange}
+        />
+        Division
+        <input
+          type="radio"
+          value="multiplication"
+          name="mathtype"
+          checked={type === "multiplication"}
+          onChange={handleTypeChange}
+        />
+        Multiplication
+      </div>
+      <div>
+        <label>Difficulty</label>
+        <br />
+        <input
+          type="radio"
+          value="easy"
+          name="difficulty"
+          checked={difficulty === "easy"}
+          onChange={handleDifficultyChange}
+        />
+        Easy
+        <input
+          type="radio"
+          value="medium"
+          name="difficulty"
+          checked={difficulty === "medium"}
+          onChange={handleDifficultyChange}
+        />
+        Medium
+        <input
+          type="radio"
+          value="hard"
+          name="difficulty"
+          checked={difficulty === "hard"}
+          onChange={handleDifficultyChange}
+        />
+        Hard
+      </div>
+    </>
   );
 };
 
--- src/components/pages/home.tsx
+++ src/components/pages/home.tsx
@@ -1,10 +1,12 @@
-import React from "react";
 import Selection from "../elements/selection";
+import Maths from "../elements/maths";
 
 const home = () => {
   return (
     <div>
       <Selection />
+      <button>Start</button>
+      <Maths />
     </div>
   );
 };
